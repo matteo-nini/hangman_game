c9bf7fb4860fd7af1056281b22d3300a19bf6f8b
Matteo <matteonini95@gmail.com>
2020-12-09 11:32:43 +0100

    [ADD] JS function for handle user select [ADD] JS function for handle a new game

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const attempSpan = document.querySelector(".attemps");
 const lettersSpan = document.querySelector(".letters");
 
 let attemps = 5;
-let errors;
+let errors = 0;
 let wordToGuess = "";
 let usedLetters = "";
 let displayWord = "";
@@ -49,4 +49,60 @@ function maskWord(word) {
   let mask = "";
   for (let i = 0; i < word.length; i++) mask += "-";
   return mask;
+}
+
+function userSelect(letter) {
+  //Check if the user just click the letter
+  if (usedLetters.indexOf(letter) != -1) {
+    return;
+  } else {
+    //add the selected letter in the selected list
+    usedLetters += letter;
+    lettersSpan.innerHTML = `${usedLetters}` + ", ";
+  }
+
+  //if the word to guess contains the letter
+  if (wordToGuess.indexOf(letter, position) != -1) {
+    let temporaryMask = displayWord; //create a temporary mask and assign it the word (maskered)
+
+    while (wordToGuess.indexOf(letter, position) != -1) {
+      position = wordToGuess.indexOf(letter, position); //than take the position of the letter
+      let end = position + 1; //and the position right next it
+
+      //create the mask from 0 to the letter position
+      let startTxt = temporaryMask.substring(0, position);
+      let endTxt = temporaryMask.substring(end, temporaryMask.length);
+      temporaryMask = startTxt + letter + endTxt;
+      position = end;
+    }
+
+    //re-assign the maskered word with the no-maskered letters
+    displayWord = temporaryMask;
+    document.game.finalWord.value = displayWord;
+
+    //If the word does not contains no more mask you win
+    if (displayWord.indexOf("-") == -1) {
+      alert("Hai vinto");
+      newGame();
+    }
+
+    //if the word to guess does not contains the letter
+  } else {
+    errors++;
+    attemps--;
+    attempSpan.innerHTML = `${attemps}`;
+
+    //check the errors (or the attemps)
+    if (attemps == 0 || errors == 5) {
+      alert("Hai perso!");
+      newGame();
+    }
+  }
+}
+
+//new game function
+function newGame() {
+  let choose = prompt("Vuoi fare un'altra partita? Y/N", "");
+  if (choose == "Y") initGame();
+  else alert("Ok! Grazie per aver giocato!");
 }
